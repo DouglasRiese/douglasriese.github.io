105573cd3eec6ef1f408a73167d0cbe0fa169857
riese2119 <riese2119@saskpolytech.ca>
2025-01-23 20:25:52 -0600

    added grid-template-rows to #app.

--- front-end/src/components/subcomponents/NavBar.vue
+++ front-end/src/components/subcomponents/NavBar.vue
@@ -4,8 +4,8 @@
 
 <template>
 <!--  example:-->
-  <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-  <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+  <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a>
+  <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>
 </template>
 
 <style scoped>
